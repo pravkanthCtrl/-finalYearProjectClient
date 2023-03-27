dc9d251f311c43c4ed810cfeb9b880119c507e98
pravkanthCtrl <125426555+pravkanthCtrl@users.noreply.github.com>
2023-03-27 18:52:00 +0530

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = ({darkMode}) => {
   return (
     <div className={darkMode?'header':'header dark'}>
-        <h3>Cows rumination process</h3>
+        <h3>CATTLE HEALTH MONITORING BY RUMINATION DETECTION </h3>
     </div>
   )
 }
